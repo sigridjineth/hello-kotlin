4c2acf5dc57e431c3ee4226bc4004ab9d76e6729
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-01-01 14:57:26 +0900

    feat: custom joinToString method

---
+++ src/main/kotlin/classPackage/joinToString.kt
@@ -0,0 +1,22 @@
+package classPackage
+
+// * Jin Hyung Park * //
+// * declares an extension function on Collection<T>.
+fun <T> Collection<T>.joinToString(
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+    // * "this" refers to the receiver of object: a collection of T. * //
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) {
+            result.append(separator)
+        }
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun String.lastChar(): Char = this[this.length - 1]
